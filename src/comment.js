471b49fd22b6bf24efc9bb3103799d38c7443084
sumin020 <128468746+sumin020@users.noreply.github.com>
2024-10-01 21:47:43 +0900

    feat: 게시글과 댓글 기능(CRUD) 수정

--- src/comment.js
+++ src/comment.js
@@ -10,84 +10,72 @@ const app = express();
 app.use(express.json());
 
 
-
 // 댓글 등록
-app.post('/api/posts/:id/comments', async (req, res) => {
+app.post('/api/posts/:id/comments', asyncHandler(async (req, res) => {
   const postId = Number(req.params.id);
-  const { nickname, content, password } = req.body;
-
-  const fields = [nickname, content, password]
-  if (fields.some(field => !field)) {
-    return res.status(400).send({ "message": "잘못된 요청입니다" });
-  };
+  const commentData = { ...req.body, postId };
 
   const newComment = await prisma.comment.create({
-    data: {
-      nickname,
-      content,
-      password,
-      postId: postId,
-    }
+    data: commentData,
+  });
+
+  await prisma.post.update({
+    where: { id: postId },
+    data: { commentCount: { increment: 1 } },
   });
 
   const { password: pwd, ...commentWithoutPassword } = newComment;
   res.status(200).send(commentWithoutPassword);
-});
+}));
+
 
 
 // 댓글 수정
-app.put('/api/comments/:id', async (req, res) => {
+app.put('/api/comments/:id', asyncHandler(async (req, res) => {
   const id = Number(req.params.id);
   const password = req.body.password;
   const comment = await prisma.comment.findUnique({
     where: { id },
   });
 
-  if (comment && comment.password === password) {
-    const updatedData = {};
-    Object.keys(req.body).forEach((key) => {
-      if (key !== 'password') {
-        updatedData[key] = req.body[key];
-      }
-    });
+  if (!comment) {
+    return res.status(404).send({ message: '존재하지 않습니다' });
+  }
 
+  if (comment.password === password) {
     const updatedComment = await prisma.comment.update({
       where : { id },
-      data: updatedData,
+      data: req.body,
     });
 
     const { password, ...commentWithoutPassword } = updatedComment;
     res.status(200).send(commentWithoutPassword);
-  } else if (comment.password !== password) {
-    res.status(403).send({ message: '비밀번호가 틀렸습니다' });
-  } else if (!comment) {
-    res.status(404).send({ message: '존재하지 않습니다' });
   } else {
-    res.status(400).send({ message: '잘못된 요청입니다' });
-  }
-});
+    res.status(403).send({ message: '비밀번호가 틀렸습니다' });
+  } 
 
+}));
 
 
 
 // 댓글 삭제
-app.delete('/api/comments/:id', async (req, res) => {
+app.delete('/api/comments/:id', asyncHandler(async (req, res) => {
   const id = Number(req.params.id);
   const password = req.body.password;
   const comment = await prisma.comment.findUnique({
     where: { id },
   });
 
-  if ( comment && comment.password === password) {
+  if (!comment) {
+    return res.status(404).send({ message: '존재하지 않습니다' });
+  }
+
+  if (comment.password === password) {
     await prisma.comment.delete({
       where: { id }, 
     });
     res.status(200).send({ message: '답글 삭제 성공' });
-  } else if (comment.password !== password) {
-    res.status(403).send({ message: '비밀번호가 틀렸습니다' });
-  } else if (!comment) {
-    res.status(404).send({ message: '존재하지 않습니다' });
   } else {
-    res.status(400).send({ message: '잘못된 요청입니다' });
-  }
-});
+    res.status(403).send({ message: '비밀번호가 틀렸습니다' });
+  } 
+}));
